b9e20f77d35cd3fd0aa5af0a6fe0a706c2b61a5a
Fiona Orinda Sugionoto <102533777+fiona-orinda@users.noreply.github.com>
2023-12-03 13:51:05 +0700

    Delete app directory

--- app/Enums/BudgetType.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Enums;
-
-enum BudgetType: int
-{
-    case Expense = 1;
-    case Saving = 2;
-}
--- app/Enums/DebtType.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Enums;
-
-enum DebtType: int
-{
-    case debt = 1;
-    case receivable = 2;
-}
